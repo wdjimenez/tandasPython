73962d846d091c8e8b094d2caabb2e2e7677dc21
wdjimenez <jiman1983mx@gmail.com>
2016-02-26 14:16:09 -0600

    Cambios a pantalla tanda

--- Qt/src/tandaView.py
+++ Qt/src/tandaView.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
@@ -7,6 +8,13 @@ import tandaController as t
 form_class_integrantes = uic.loadUiType("tandaView.ui")[0]
 
 class tandaViewClass(QtGui.QDialog, form_class_integrantes):
-	def __init__(self, parent=None):
-		QtGui.QDialog.__init__(self, parent)
-		self.setupUi(self)  		
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+        self.setupUi(self)      
+        tanda = t.tandaController()
+        self._periodicidad = tanda.recuperarPeriodicidad()
+        self._integrantes = tanda.recuperarIntegrantes()
+        for per in self._periodicidad:
+            self.listWidgetPeriodo.addItem(per[1])
+        for integrante in self._integrantes:
+            self.listWidgetIntegrantes.addItem(str(integrante[1]) + ' ' + str(integrante[2]))
